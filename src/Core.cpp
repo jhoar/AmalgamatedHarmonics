6a5a0fca89ae6adb8f2fa459653ba3ea6d082743
John Hoar <johnhoar@yahoo.co.uk>
2017-11-21 10:56:45 +0100

    Refactor code and start on Arp

---
+++ src/Core.cpp
@@ -0,0 +1,132 @@
+#include "Core.hpp"
+
+#include <iostream>
+
+float Quantizer::getPitchFromVolts(float inVolts, float inRoot, float inScale, int *outRoot, int *outScale, int *outNote, int *outDegree) {
+	
+	stepX++;
+		
+	// get the root note and scale
+	int currRoot = round(rescalef(fabs(inRoot), 0, 10, 0, Quantizer::NUM_NOTES));
+	int currScale = round(rescalef(fabs(inScale), 0, 10, 0, Quantizer::NUM_SCALES));
+	int currDegree = 0;	
+
+	int *curScaleArr;
+	int notesInScale = 0;
+	switch (currScale){
+		case CHROMATIC:			curScaleArr = SCALE_CHROMATIC;		notesInScale=LENGTHOF(SCALE_CHROMATIC); break;
+		case IONIAN:			curScaleArr = SCALE_IONIAN;			notesInScale=LENGTHOF(SCALE_IONIAN); break;
+		case DORIAN:			curScaleArr = SCALE_DORIAN;			notesInScale=LENGTHOF(SCALE_DORIAN); break;
+		case PHRYGIAN:			curScaleArr = SCALE_PHRYGIAN;		notesInScale=LENGTHOF(SCALE_PHRYGIAN); break;
+		case LYDIAN:			curScaleArr = SCALE_LYDIAN;			notesInScale=LENGTHOF(SCALE_LYDIAN); break;
+		case MIXOLYDIAN:		curScaleArr = SCALE_MIXOLYDIAN;		notesInScale=LENGTHOF(SCALE_MIXOLYDIAN); break;
+		case AEOLIAN:			curScaleArr = SCALE_AEOLIAN;		notesInScale=LENGTHOF(SCALE_AEOLIAN); break;
+		case LOCRIAN:			curScaleArr = SCALE_LOCRIAN;		notesInScale=LENGTHOF(SCALE_LOCRIAN); break;
+		case MAJOR_PENTA:		curScaleArr = SCALE_MAJOR_PENTA;	notesInScale=LENGTHOF(SCALE_MAJOR_PENTA); break;
+		case MINOR_PENTA:		curScaleArr = SCALE_MINOR_PENTA;	notesInScale=LENGTHOF(SCALE_MINOR_PENTA); break;
+		case HARMONIC_MINOR:	curScaleArr = SCALE_HARMONIC_MINOR;	notesInScale=LENGTHOF(SCALE_HARMONIC_MINOR); break;
+		case BLUES:				curScaleArr = SCALE_BLUES;			notesInScale=LENGTHOF(SCALE_BLUES); break;
+		default: 				curScaleArr = SCALE_CHROMATIC;		notesInScale=LENGTHOF(SCALE_CHROMATIC);
+	}
+
+	// get the octave
+	int octave = floor(inVolts);
+	float closestVal = 10.0;
+	float closestDist = 10.0;
+	int noteFound = 0;
+
+	if (debug && stepX % poll == 0) {
+		std::cout << "Octave: " << octave << " Scale: " << scaleNames[currScale] << " Root: " << noteNames[currRoot] << std::endl;
+	}
+
+	for (int i = 0; i < notesInScale; i++) {
+
+		float fOctave = (float)octave;
+		int iDegree = curScaleArr[i];
+		float fVoltsAboveOctave = iDegree / 12.0;
+		float fScaleNoteInVolts = fOctave + fVoltsAboveOctave;
+		float distAway = fabs(inVolts - fScaleNoteInVolts);
+
+		if (debug && stepX % poll == 0) {
+			std::cout << "input: " << inVolts 
+			<< " index: " << i 
+			<< " root: " << currRoot
+			<< " octave: " << fOctave
+			<< " degree: " << iDegree
+			<< " V above O: " << fVoltsAboveOctave
+			<< " note in V: " << fScaleNoteInVolts
+			<< " distance: " << distAway
+			<< std::endl;
+		}
+
+		// Assume that the list of notes is ordered, so there is an single inflection point at the minimum value 
+		if (distAway > closestDist){
+			noteFound = i - 1; // We break here because the previous note was closer, all subsequent notes are father away
+			currDegree = iDegree;
+			break;
+		} else {
+			// Let's remember this
+			closestVal = fScaleNoteInVolts;
+			closestDist = distAway;
+		}
+	}
+
+	int currNote = (currRoot + curScaleArr[noteFound]) % 12;
+	if (debug && stepX % poll == 0) {
+		// Dump the note and degree, mod the size in case where we have wrapped round
+
+		std::cout << "Found note in scale: " << noteFound 
+		<< " Scale degree: " << curScaleArr[noteFound] << " " << degreeNames[curScaleArr[noteFound]] 
+		<< " Note: " << currNote << " " << noteNames[currNote] 
+		<< std::endl;
+	}
+
+	*outRoot = currRoot;
+	*outScale = currScale;
+	*outNote = currNote;
+	*outDegree = currDegree;
+	
+	return closestVal;
+ 
+}
+
+void Quantizer::calculateKey(int inKey, float spacing, float xOff, float yOff, float *x, float *y, int *scale) {
+
+	// Sanitise input
+	int key = inKey % 12;
+
+	// White
+	int whiteO [7] = {0, 1, 2, 3, 4, 5, 6};		// 'spaces' occupied by keys
+	int whiteK [7] = {0, 2, 4, 5, 7, 9, 11};	// scale mapped to key
+	// Black
+	int blackO [5] = {0, 1, 3, 4, 5};			// 'spaces' occupied by keys
+	int blackK [5] = {1, 3, 6, 8, 10};			// scale mapped to keys
+
+	float bOffset = xOff + (spacing / 2);
+	float bSpace = spacing;
+	// Make an equilateral triangle pattern, tri height = base * (sqrt(3) / 2)
+	float bDelta = bSpace * sqrt(3) * 0.5;
+
+	// Are we black or white key?, check black keys
+	for (int i = 0; i < 5; i++) {
+		if (blackK[i] == key) {
+			*x = bOffset + bSpace * blackO[i];
+			*y = yOff - bDelta;
+			*scale = blackK[i];
+			return;
+		}
+	}
+
+	int wOffset = xOff;
+	int wSpace = spacing;
+	
+	for (int i = 0; i < 7; i++) {
+		if (whiteK[i] == key) {
+			*x = wOffset + wSpace * whiteO[i];
+			*y = yOff;
+			*scale = whiteK[i];
+			return;
+		}
+	}
+}
+
---
+++ src/Core.hpp
@@ -0,0 +1,142 @@
+#pragma once
+
+#include "AH.hpp"
+
+#include <iostream>
+
+struct Quantizer {
+
+	// Reference, midi note to scale
+	// 0	1
+	// 1	b2 (#1)
+	// 2	2
+	// 3	b3 (#2)
+	// 4	3
+	// 5	4
+	// 6	b5 (#4)
+	// 7	5
+	// 8	b6 (#5)
+	// 9 	6
+	// 10	b7 (#6)
+	// 11	7
+
+	// http://www.grantmuller.com/MidiReference/doc/midiReference/ScaleReference.html
+	// Although their definition of the Blues scale is wrong
+	// Added the octave note to ensure that the last note is correctly processed
+	int SCALE_CHROMATIC      [13]= {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}; 	// All of the notes
+	int SCALE_IONIAN         [8] = {0, 2, 4, 5, 7, 9, 11, 12};			// 1,2,3,4,5,6,7
+	int SCALE_DORIAN         [8] = {0, 2, 3, 5, 7, 9, 10, 12};			// 1,2,b3,4,5,6,b7
+	int SCALE_PHRYGIAN       [8] = {0, 1, 3, 5, 7, 8, 10, 12};			// 1,b2,b3,4,5,b6,b7
+	int SCALE_LYDIAN         [8] = {0, 2, 4, 6, 7, 9, 10, 12};			// 1,2,3,#4,5,6,7
+	int SCALE_MIXOLYDIAN     [8] = {0, 2, 4, 5, 7, 9, 10, 12};			// 1,2,3,4,5,6,b7 
+	int SCALE_AEOLIAN        [8] = {0, 2, 3, 5, 7, 8, 10, 12};			// 1,2,b3,4,5,b6,b7
+	int SCALE_LOCRIAN        [8] = {0, 1, 3, 5, 6, 8, 10, 12};			// 1,b2,b3,4,b5,b6,b7
+	int SCALE_MAJOR_PENTA    [6] = {0, 2, 4, 7, 9, 12};					// 1,2,3,5,6
+	int SCALE_MINOR_PENTA    [6] = {0, 3, 5, 7, 10, 12};				// 1,b3,4,5,b7
+	int SCALE_HARMONIC_MINOR [8] = {0, 2, 3, 5, 7, 8, 11, 12};			// 1,2,b3,4,5,b6,7
+	int SCALE_BLUES          [7] = {0, 3, 5, 6, 7, 10, 12};				// 1,b3,4,b5,5,b7
+
+	enum Notes {
+		NOTE_C,
+		NOTE_D_FLAT,
+		NOTE_D,
+		NOTE_E_FLAT,
+		NOTE_E,
+		NOTE_F,
+		NOTE_G_FLAT,
+		NOTE_G,
+		NOTE_A_FLAT,
+		NOTE_A,
+		NOTE_B_FLAT,
+		NOTE_B,
+		NUM_NOTES
+	};
+
+	std::string noteNames[12] = {
+		"C",
+		"Db",
+		"D",
+		"Eb",
+		"E",
+		"F",
+		"Gb",
+		"G",
+		"Ab",
+		"A",
+		"Bb",
+		"B",
+	};
+
+	Notes root = NOTE_C;
+
+	enum Scales {
+		CHROMATIC,
+ 		IONIAN,
+		DORIAN,
+		PHRYGIAN,
+		LYDIAN,
+		MIXOLYDIAN,
+		AEOLIAN,
+		LOCRIAN,
+		MAJOR_PENTA,
+		MINOR_PENTA,
+		HARMONIC_MINOR,
+		BLUES,
+		NUM_SCALES
+	};
+
+	std::string scaleNames[12] = {
+		"Chromatic",
+		"Ionian (Major)",
+		"Dorian",
+		"Phrygian",
+		"Lydian",
+		"Mixolydian",
+		"Aeolian (Natural Minor)",
+		"Locrian",
+		"Major Pentatonic",
+		"Minor Pentatonic",
+		"Harmonic Minor",
+		"Blues"
+	};
+
+	enum Degrees {
+		FIRST,
+		FLAT_SECOND,
+		SECOND,
+		FLAT_THIRD,
+		THIRD,
+		FOURTH,
+		FLAT_FIFTH,
+		FIFTH,
+		FLAT_SIXTH,
+		SIXTH,
+		FLAT_SEVENTH,
+		SEVENTH,
+		OCTAVE
+	};
+
+	std::string degreeNames[13] {
+		"1",
+		"b2",
+		"2",
+		"b3",
+		"3",
+		"4",
+		"b5",
+		"5",
+		"b6",
+		"6",
+		"b7",
+		"7",
+		"O"
+	};
+
+	bool debug = false;
+	int poll = 50000;
+	int stepX = 0;
+
+	float getPitchFromVolts(float inVolts, float inRoot, float inScale, int *outRoot, int *outScale, int *outNote, int *outDegree);
+	void calculateKey(int inKey, float spacing, float xOff, float yOff, float *x, float *y, int *scale); 
+};
+
