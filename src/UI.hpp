aff189080c9ce6f2913f4cb81c7fa283aeb7cc22
John Hoar <johnhoar@yahoo.co.uk>
2018-03-05 22:55:21 +0100

    Finish first batch of UI tweaks

--- src/UI.hpp
+++ src/UI.hpp
@@ -153,29 +153,29 @@ struct UI {
 		TRIMPOT
 	};
 	
-	float Y_KNOB[2]    = {49.0, 56.0};
-	float Y_PORT[2]    = {49.0, 56.0};
-	float Y_BUTTON[2]  = {52.0, 56.0};
-	float Y_LIGHT[2]   = {56.4, 56.0};
-	float Y_TRIMPOT[2] = {56.4, 56.0};
+	float Y_KNOB[2]    = {50.8, 56.0}; // w.r.t 22 = 28.8 from bottom
+	float Y_PORT[2]    = {49.7, 56.0}; // 27.7
+	float Y_BUTTON[2]  = {53.3, 56.0}; // 31.3 
+	float Y_LIGHT[2]   = {57.7, 56.0}; // 35.7
+	float Y_TRIMPOT[2] = {52.8, 56.0}; // 30.8
 
-	float Y_KNOB_COMPACT[2]     = {29.0, 35.0};
+	float Y_KNOB_COMPACT[2]     = {30.1, 35.0}; // Calculated relative to  PORT=29 and the deltas above
 	float Y_PORT_COMPACT[2]     = {29.0, 35.0};
-	float Y_BUTTON_COMPACT[2]   = {32.0, 35.0};
-	float Y_LIGHT_COMPACT[2]    = {36.4, 35.0};
-	float Y_TRIMPOT_COMPACT[2]  = {36.4, 35.0};
+	float Y_BUTTON_COMPACT[2]   = {32.6, 35.0};
+	float Y_LIGHT_COMPACT[2]    = {37.0, 35.0};
+	float Y_TRIMPOT_COMPACT[2]  = {32.1, 35.0};
 	
-	float X_KNOB[2]     = {11.5, 48.0};
-	float X_PORT[2]     = {11.5, 48.0};
-	float X_BUTTON[2]   = {14.5, 48.0};
-	float X_LIGHT[2]    = {18.9, 48.0};
-	float X_TRIMPOT[2]  = {18.9, 48.0};
+	float X_KNOB[2]     = {12.5, 48.0}; // w.r.t 6.5 = 6 from left
+	float X_PORT[2]     = {11.5, 48.0}; // 5
+	float X_BUTTON[2]   = {14.7, 48.0}; // 8.2
+	float X_LIGHT[2]    = {19.1, 48.0}; // 12.6
+	float X_TRIMPOT[2]  = {14.7, 48.0}; // 8.2
 
-	float X_KNOB_COMPACT[2]     = {20.0, 35.0};
-	float X_PORT_COMPACT[2]     = {19.0, 35.0};
-	float X_BUTTON_COMPACT[2]   = {22.0, 35.0};
-	float X_LIGHT_COMPACT[2]    = {26.4, 35.0};
-	float X_TRIMPOT_COMPACT[2]  = {27.4, 35.0};
+	float X_KNOB_COMPACT[2]     = {21.0, 35.0}; // 15 + 6, see calc above
+	float X_PORT_COMPACT[2]     = {20.0, 35.0}; // 15 + 5
+	float X_BUTTON_COMPACT[2]   = {23.2, 35.0}; // 15 + 8.2
+	float X_LIGHT_COMPACT[2]    = {27.6, 35.0}; // 15 + 12.6
+	float X_TRIMPOT_COMPACT[2]  = {23.2, 35.0}; // 15 + 8.2
 	
 
 	Vec getPosition(int type, int xSlot, int ySlot, bool xDense, bool yDense);
