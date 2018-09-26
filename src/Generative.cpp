5621243eca8e8db002e81402429b533ab19dfb6f
John Hoar <johnhoar@yahoo.co.uk>
2018-09-26 12:58:30 +0200

    Add CV offset

--- src/Generative.cpp
+++ src/Generative.cpp
@@ -7,12 +7,16 @@
 #include "Core.hpp"
 #include "UI.hpp"
 
-// TODO delay control, 0-10v scaling, UI, random gate length, wave range map to -5-5v
+// TODO delay control, UI, random gate length, wave range map to -5-5v
 
+// Andrew Belt's LFO-2 code
 struct LowFrequencyOscillator {
 	float phase = 0.0f;
 	float pw = 0.5f;
 	float freq = 1.0f;
+	bool offset = false;
+	bool invert = false;
+	SchmittTrigger resetTrigger;
 
 	LowFrequencyOscillator() {}
 	void setPitch(float pitch) {
@@ -23,6 +27,11 @@ struct LowFrequencyOscillator {
 		const float pwMin = 0.01f;
 		pw = clamp(pw_, pwMin, 1.0f - pwMin);
 	}
+	void setReset(float reset) {
+		if (resetTrigger.process(reset / 0.01f)) {
+			phase = 0.0f;
+		}
+	}
 	void step(float dt) {
 		float deltaPhase = fminf(freq * dt, 0.5f);
 		phase += deltaPhase;
@@ -30,28 +39,37 @@ struct LowFrequencyOscillator {
 			phase -= 1.0f;
 	}
 	float sin() {
-		return sinf(2*M_PI * phase);
+		if (offset)
+			return 1.0f - cosf(2*M_PI * phase) * (invert ? -1.0f : 1.0f);
+		else
+			return sinf(2*M_PI * phase) * (invert ? -1.0f : 1.0f);
 	}
 	float tri(float x) {
 		return 4.0f * fabsf(x - roundf(x));
 	}
 	float tri() {
-		return -1.0f + tri(phase - 0.75f);
+		if (offset)
+			return tri(invert ? phase - 0.5f : phase);
+		else
+			return -1.0f + tri(invert ? phase - 0.25f : phase - 0.75f);
 	}
 	float saw(float x) {
 		return 2.0f * (x - roundf(x));
 	}
 	float saw() {
-		return saw(phase);
+		if (offset)
+			return invert ? 2.0f * (1.0f - phase) : 2.0f * phase;
+		else
+			return saw(phase) * (invert ? -1.0f : 1.0f);
 	}
 	float sqr() {
-		return (phase < pw) ? 1.0f : -1.0f;
+		float sqr = (phase < pw) ^ invert ? 1.0f : -1.0f;
+		return offset ? sqr + 1.0f : sqr;
 	}
 	float light() {
 		return sinf(2*M_PI * phase);
 	}
 };
-
 struct Generative : AHModule {
 
 	enum ParamIds {
@@ -87,9 +105,7 @@ struct Generative : AHModule {
 		NUM_LIGHTS
 	};
 
-	Generative() : AHModule(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {
-
-	}
+	Generative() : AHModule(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {}
 	
 	void step() override;
 
@@ -105,50 +121,55 @@ struct Generative : AHModule {
 	float target = 0.0f;
 	float current = 0.0f;
 	bool quantise = false;
+	bool offset = false;
 	bool delayState = false;
 	
 	// minimum and maximum slopes in volts per second
 	const float slewMin = 0.1;
 	const float slewMax = 10000.0;
-	
 	const float slewRatio = slewMin / slewMax;
 	
 	// Amount of extra slew per voltage difference
-	const float shapeScale = 1.0/10.0;
+	const float shapeScale = 1.0 / 10.0;
 	
 };
-
 
 void Generative::step() {
 	
 	AHModule::step();
 
 	oscillator.setPitch(params[FREQ_PARAM].value + params[FM_PARAM].value * inputs[FM_INPUT].value);
+	oscillator.offset = offset;
 	oscillator.step(delta);
 
 	float wave = clamp(params[WAVE_PARAM].value + inputs[WAVE_INPUT].value, 0.0f, 3.0f);
 
-	float interp;
+	float interp = 0.0f;
 	bool toss = false;
 
 	if (wave < 1.0f)
-		interp = crossfade(oscillator.sin(), oscillator.tri(), wave) * 5.0; // SCALING
+		interp = crossfade(oscillator.sin(), oscillator.tri(), wave) * 5.0; 
 	else if (wave < 2.0f)
-		interp = crossfade(oscillator.tri(), oscillator.saw(), wave - 1.0f) * 5.0; // SCALING
+		interp = crossfade(oscillator.tri(), oscillator.saw(), wave - 1.0f) * 5.0;
 	else
-		interp = crossfade(oscillator.saw(), oscillator.sqr(), wave - 2.0f) * 5.0; // SCALING
+		interp = crossfade(oscillator.saw(), oscillator.sqr(), wave - 2.0f) * 5.0;
 
 	// Capture (pink) noise
 	float noise = clamp(pink.next() * 7.5f, -5.0f, 5.0f); // SCALING
 	float range = params[RANGE_PARAM].value;
 
 	// Mix AM (noise if no AM input) with LFO and scale
-	// TODO FIX SCALING (check unidir 0-10, we assume -5 to 5 here)	
 	float mixedSignal;
+	float noiseOffset = 0.0f;
+
+	if (offset) {
+		noiseOffset = 5.0f;
+	} 
+
 	if (inputs[AM_INPUT].active) {
 	 	mixedSignal = crossfade(interp, inputs[AM_INPUT].value, params[AM_PARAM].value) * range;
 	} else {
-	 	mixedSignal = (noise * params[AM_PARAM].value + interp * (1.0f - params[AM_PARAM].value)) * range;
+	 	mixedSignal = ((noise + noiseOffset) * params[AM_PARAM].value + interp * (1.0f - params[AM_PARAM].value)) * range;
 	}
 
 	// Process gate
@@ -171,7 +192,6 @@ void Generative::step() {
 			float delayTime = clamp(dlySpr * rndD, 0.0f, 100.0f);
 		
 			// Trigger the respective delay pulse generator
-
 			delayState = true;
 			delayPhase.trigger(delayTime);
 
@@ -182,14 +202,13 @@ void Generative::step() {
 		delayState = false;
 
 		float threshold = clamp(params[PROB_PARAM].value + inputs[PROB_INPUT].value / 10.f, 0.f, 1.f);
-		toss = randomUniform() < threshold;
+		toss = (randomUniform() < threshold);
 
 		if (toss) {
 			target = mixedSignal;
 			tossPulse.trigger(Core::TRIGGER);
 		}
 	}
-
 
 	if (!hold) {
 		// Rise
@@ -281,10 +300,21 @@ struct GenerativeWidget : ModuleWidget {
 				}
 			};
 
+			struct GenOffsetItem : MenuItem {
+				Generative *gen;
+				void onAction(EventAction &e) override {
+					gen->offset ^= 1;
+				}
+				void step() override {
+					rightText = gen->offset ? "0V - 10V" : "-5V to 5V";
+					MenuItem::step();
+				}
+			};
+
 			menu->addChild(construct<MenuLabel>());
 			menu->addChild(construct<GenModeItem>(&MenuItem::text, "Quantise", &GenModeItem::gen, gen));
+			menu->addChild(construct<GenOffsetItem>(&MenuItem::text, "CV Offset", &GenOffsetItem::gen, gen));
 	}
 };
 
 Model *modelGenerative = Model::create<Generative, GenerativeWidget>( "Amalgamated Harmonics", "Generative", "Generative", NOISE_TAG);
-
