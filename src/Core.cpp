dd365aa849211c1f353b9cd16a0c429a58160f14
John Hoar <johnhoar@yahoo.co.uk>
2018-10-10 00:36:58 +0200

    Use local Pi definition

--- src/Core.cpp
+++ src/Core.cpp
@@ -178,9 +178,9 @@ void Core::getRootFromMode(int inMode, int inRoot, int inTonic, int *currRoot, i
  	if(phase == 0) {
  		U = (rand() + 1.) / (RAND_MAX + 2.);
  		V = rand() / (RAND_MAX + 1.);
- 		Z = sqrt(-2 * log(U)) * sin(2 * M_PI * V);
+ 		Z = sqrt(-2 * log(U)) * sin(2 * PI * V);
  	} else
- 		Z = sqrt(-2 * log(U)) * cos(2 * M_PI * V);
+ 		Z = sqrt(-2 * log(U)) * cos(2 * PI * V);
 
  	phase = 1 - phase;
 
