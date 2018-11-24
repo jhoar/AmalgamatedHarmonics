8c7178393c753fe236a8cd029a0b1bf80907d493
John Hoar <johnhoar@yahoo.co.uk>
2018-11-24 17:42:55 +0100

    Add spread control

--- src/Chord.cpp
+++ src/Chord.cpp
@@ -19,6 +19,7 @@ struct Chord : AHModule {
 		ENUMS(PW_PARAM,6),
 		ENUMS(PWM_PARAM,6),
 		ENUMS(ATTN_PARAM,6),
+		SPREAD_PARAM,
 		NUM_PARAMS
 	};
 	enum InputIds {
@@ -34,7 +35,11 @@ struct Chord : AHModule {
 		NUM_LIGHTS
 	};
 	
-	Chord() : AHModule(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {}
+	Chord() : AHModule(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {
+		for(int i = 0; i < 6; i++) {
+			voicePosRad[i] = voicePosDeg[i] * 0.5 * PI_180;
+		}
+	}
 
 	void step() override;
 		
@@ -47,6 +52,8 @@ struct Chord : AHModule {
 
 	EvenVCO oscillator[6];
 
+	float voicePosDeg[6] = {-90.0f, 90.0f, -54.0f, 54.0f, -18.0f, 18.0f};
+	float voicePosRad[6];	
 };
 
 void Chord::step() {
@@ -54,17 +61,18 @@ void Chord::step() {
 	AHModule::step();
 
 	float out[2];
-	int nPitches[2];
+	int nPitches = 0;
 
 	out[0] = 0.0f;
 	out[1] = 0.0f;
-	nPitches[0] = 0;
-	nPitches[1] = 0;
+
+	float spread = params[SPREAD_PARAM].value;
+	float SQRT2_2 = sqrt(2.0) / 2.0;
+	float PI_180 = PI / 180.0;
 
 	for (int i = 0; i < NUM_PITCHES; i++) {
 
 		int index = PITCH_INPUT + i;
-		int side = i % 2;
 
 		float pitchCv = inputs[index].value + params[OCTAVE_PARAM + i].value;
 		float pitchFine = params[DETUNE_PARAM + i].value / 12.0; // +- 1V
@@ -74,29 +82,31 @@ void Chord::step() {
 
 		if (inputs[index].active) {
 
-			nPitches[side]++;
-			
+			float amp = 0.0;
+			nPitches++;
+
 			int wave = params[WAVE_PARAM + i].value;
 			switch(wave) {
-				case 0:		out[side] += oscillator[i].sine * attn;			break;
-				case 1:		out[side] += oscillator[i].saw * attn;			break;
-				case 2:		out[side] += oscillator[i].doubleSaw * attn;	break;
-				case 3:		out[side] += oscillator[i].square * attn;		break;
-				case 4:		out[side] += oscillator[i].even * attn;			break;
-				default:	out[side] += oscillator[i].sine * attn;			break;
+				case 0:		amp = oscillator[i].sine * attn;		break;
+				case 1:		amp = oscillator[i].saw * attn;			break;
+				case 2:		amp = oscillator[i].doubleSaw * attn;	break;
+				case 3:		amp = oscillator[i].square * attn;		break;
+				case 4:		amp = oscillator[i].even * attn;		break;
+				default:	amp = oscillator[i].sine * attn;		break;
 			};
+
+			float angle = spread * voicePosRad[i];
+			float left = SQRT2_2 * (cos(angle) - sin(angle));
+    		float right = SQRT2_2 * (cos(angle) + sin(angle));
+
+			out[0] += left * amp;
+			out[1] += right * amp;
+
 		}
 	}
 
-	// Left
-	if (nPitches[0] > 0) {
-		out[0] = (out[0] / nPitches[0]) * 5.0;
-	}
-
-	// Right
-	if (nPitches[1] > 0) {
-		out[1] = (out[1] / nPitches[1]) * 5.0;
-	}
+	out[0] = (out[0] * 5.0f) / nPitches;
+	out[1] = (out[1] * 5.0f) / nPitches;
 
 	if (outputs[OUT_OUTPUT].active && outputs[OUT_OUTPUT + 1].active) {
 		outputs[OUT_OUTPUT].value 		= out[0];
@@ -142,6 +152,7 @@ struct ChordWidget : ModuleWidget {
 			addParam(ParamWidget::create<AHKnobNoSnap>(ui.getPosition(UI::KNOB, n, 7, true, true), module, Chord::ATTN_PARAM + n, 0.0f, 1.0f, 1.0f));
 		}
 
+		addParam(ParamWidget::create<AHKnobNoSnap>(ui.getPosition(UI::KNOB, 0, 5, true, false), module, Chord::SPREAD_PARAM, 0.0f, 1.0f, 1.0f));
 
 		addOutput(Port::create<PJ301MPort>(ui.getPosition(UI::PORT, 4, 5, true, false), Port::OUTPUT, module, Chord::OUT_OUTPUT));
 		addOutput(Port::create<PJ301MPort>(ui.getPosition(UI::PORT, 5, 5, true, false), Port::OUTPUT, module, Chord::OUT_OUTPUT + 1));
